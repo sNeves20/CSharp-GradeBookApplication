fc9d608da39290bf6f95c104501de6ff041ad232
sNeves20 <sergomgn@hotmai.com>
2020-10-12 13:20:40 +0100

    fixed bug

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -1,15 +1,11 @@
-﻿using GradeBook.Enums;
-using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace GradeBook.GradeBooks
 {
     class StandardGradeBook : BaseGradeBook
     {
         public StandardGradeBook(string name) : base(name)
         {
-            Type = GradeBookType.Standard;
+            Type = Enums.GradeBookType.Standard;
         }
     }
 }
