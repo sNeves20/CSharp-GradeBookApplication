ad563b447b41c4d0786b201909e0785420b21793
sNeves20 <sergomgn@hotmai.com>
2020-10-12 15:39:54 +0100

    Getter Letter grade in RankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,4 +1,8 @@
-﻿namespace GradeBook.GradeBooks
+﻿using System.Globalization;
+using System;
+using System.Linq;
+
+namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
@@ -8,12 +12,26 @@
         }
 
         public override char GetLetterGrade(double averageGrade)
-        {   
-            if(base.Students.Count < 5)
-            {
+        {
+
+            var StudentNumber = base.Students.Count;
+            if(StudentNumber < 5)
+                throw new InvalidOperationException("Ranked Grading Requires at least 5 students");
+
+            var treshhold = (int)Math.Ceiling(StudentNumber * 0.2); // Since we want to grade students divided into 5 groups (A,B,C,D,F) 0.2 = 1/5
+
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[treshhold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[(treshhold * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (grades[(treshhold * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (grades[(treshhold * 4) - 1] <= averageGrade)
+                return 'D';
+            else
                 return 'F';
-            }
-            return base.GetLetterGrade(averageGrade);
         }
     }
 }
