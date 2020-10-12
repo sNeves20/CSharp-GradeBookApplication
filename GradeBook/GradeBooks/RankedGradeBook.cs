245bf7de9306357023b750f184b4b6ca7b8379ba
sNeves20 <sergomgn@hotmai.com>
2020-10-12 15:17:23 +0100

    Getter Letter grade in RankedGradeBook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -6,5 +6,14 @@
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {   
+            if(base.Students.Count < 5)
+            {
+                return 'F';
+            }
+            return base.GetLetterGrade(averageGrade);
+        }
     }
 }
